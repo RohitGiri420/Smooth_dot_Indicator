8e19733139a0b940c95261c66906bc107bb4947c
Rohit Giri <giri420rohit@gmail.com>
2024-04-28 17:48:19 +0530

    Pages created

---
+++ android/app/src/main/kotlin/com/giri/smooth_dot_indicator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.giri.smooth_dot_indicator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/Pages/Page1.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Page1 extends StatelessWidget {
+  const Page1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ClipRect(
+          child: Container(
+            height: 400,
+            width: 250,
+            color: Colors.deepPurple,
+          ),
+        ),
+      ),
+    );
+  }
+}
